75c0057162273c42b41509656bb7a9d8c4b3c403
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-26 20:27:46 +0530

    Added all imp states and useEffects

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from '../Task/Task';
 
 const Home = () => {
+  const pendingTask = localStorage.getItem("tasks")? JSON.parse(localStorage.getItem("tasks")) : [];
+
+  const [tasks,setTasks] =useState(pendingTask);
+  const [title,setTitle] = useState("");
+  const [description,setDescription] = useState("");
+
+  const submitHandler =(e)=>{
+    e.preventDefault();
+
+    setTasks([...tasks,{
+      title:title,
+      description:description
+    }]);
+
+    setTitle("");
+    setDescription("");
+
+  }
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  },[tasks]);
+
+  const deleteTask = (index)=>{
+    const newArr = tasks.filter((_,i)=>{
+      return i!==index;
+    })
+
+    setTasks(newArr)
+  }
   return (
     <div className='container'>
       <h1>Daliy Goals</h1>
-        <form action="">
-            <input type="text" placeholder='Title' />
-            <textarea name="" id="" placeholder='Description'></textarea>
+        <form onSubmit={submitHandler}>
+            <input type="text" placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            <textarea name="" id="" placeholder='Description' value={description} onChange={(e)=>setDescription(e.target.value)}></textarea>
             <button type='submit'>Add</button>
         </form>
-        <Task />
-      
+        {tasks.map((item,index)=>(
+          <Task key={index} 
+          title={item.title} 
+          description={item.description}
+          deleteTask={deleteTask}
+          index={index}/>
+        ))}   
     </div>
   )
 }
